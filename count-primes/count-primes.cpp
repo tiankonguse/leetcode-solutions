dfdc6e56ed146214aa09afd7cc4b784b6ba5495d
tiankonguse <i@tiankonguse.com>
2015-05-19 23:56:22 +0800

    add solution : regular-expression-matching

---
+++ count-primes/count-primes.cpp
@@ -0,0 +1,36 @@
+/*************************************************************************
+    > File Name: count-primes.cpp
+    > Author: tiankonguse
+    > Mail: i@tiankonguse.com 
+    > Created Time: 2015年05月06日 星期三 23时48分42秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<string>
+#include<stack>
+#include<map>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<time.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+            
+    }
+};
+
+void test(){
+    srand(time(NULL));
+    Solution work(1);
+}
+
+int main(){
+    test();
+    return 0;
+}
---
+++ count-primes/readme.md
@@ -0,0 +1,12 @@
+# Count Primes
+
+Description:
+
+Count the number of prime numbers less than a non-negative number, n
+
+
+References:
+
+[How Many Primes Are There?](https://primes.utm.edu/howmany.html)
+
+[Sieve of Eratosthenes](http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
