38f9f6b35f3fc4f33c46b59bbd9abd858df30f7a
tiankonguse <i@tiankonguse.com>
2015-05-21 21:02:39 +0800

    update

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+  > File Name: letter-combinations-of-a-phone-number.cpp
+  > Author: tiankonguse(skyyuan)
+  > Mail: i@tiankonguse.com 
+  > Created Time: 2015年05月21日 20:42:13
+***********************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<string>
+#include<queue>
+#include<map>
+#include<cmath>
+#include<stack>
+#include<algorithm>
+#include<functional>
+#include<stdarg.h>
+using namespace std;
+#ifdef __int64
+typedef __int64 LL;
+#else
+typedef long long LL;
+#endif
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        
+    }
+};
+int main() {
+
+    return 0;
+}
---
+++ letter-combinations-of-a-phone-number/readme.md
@@ -0,0 +1,18 @@
+# Letter Combinations of a Phone Number
+
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+![letter-combinations-of-a-phone-number](http://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Telephone-keypad2.svg/200px-Telephone-keypad2.svg.png)
+
+```
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+```
+
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+
+
