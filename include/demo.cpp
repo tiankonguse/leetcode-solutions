b4ec8211597162434899c0d9aa212a596cd0a0c9
tiankonguse <i@tiankonguse.com>
2019-04-27 14:10:02 +0800

    update

--- include/demo.cpp
+++ include/demo.cpp
@@ -13,18 +13,6 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int K) {
-        int sum = 0;
-        set<int> have;
-        for(int i=1;;i++){
-            sum = (sum * 10 + 1)%K;
-            if(sum == 0){
-                return i;
-            }
-            if(have.find(sum) != have.end()){
-                return -1;
-            }
-            have.insert(sum);
-        }
         return -1;
     }
 };
@@ -32,20 +20,6 @@ public:
 /*
     //ListNode* root = vecToList({1,2,3,4});
     // vecToTree({});
-
-    #define CLASS Solution
-    #define FUNCTION smallestRepunitDivByK
-
-    int first;
-    int second;
-    int expectAns;
-
-
-    first = 1;
-    second = 1;
-    expectAns = 1;
-    TEST_SMP1(CLASS, FUNCTION, expectAns, first);
-    //TEST_SMP2(Solution, FUNCTION, expectAns, first, second);
 */
 int main() {
     #define CLASS Solution
@@ -57,7 +31,7 @@ int main() {
 
     first = 113;
     second = 1;
-    expectAns = 112;
+    expectAns = -1;
     TEST_SMP1(CLASS, FUNCTION, expectAns, first);
     //TEST_SMP2(Solution, FUNCTION, expectAns, first, second);
 
