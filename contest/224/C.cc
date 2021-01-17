2dfb241470e5084c35d34760371995027183a21c
tiankonguse <i@tiankonguse.com>
2021-01-17 17:21:10 +0800

    update

--- contest/224/C.cc
+++ contest/224/C.cc
@@ -23,8 +23,39 @@ const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
 const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
 class Solution {
 public:
-    vector<int> decode(vector<int>& encoded, int a) {
-        vector<int> ans;
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int ans = 0;
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<pair<int, int>> dp(m);
+        for(int i=0;i<m;i++){
+            dp[i] = {i, 0};
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]){
+                    dp[j].second++;
+                }else{
+                    dp[j].second = 0;
+                }
+            }
+
+            sort(dp.begin(), dp.end(), [](const pair<int, int>&a, const pair<int, int>&b){
+                return a.second > b.second;
+            });
+
+            for(int j=0;j<m;j++){
+                ans = max(ans, dp[j].second * (j+1));
+            }
+            sort(dp.begin(), dp.end(), [](pair<int, int>&a, pair<int, int>&b){
+                return a.first < b.first;
+            });
+        }
+
+
         return ans;
     }
 };
