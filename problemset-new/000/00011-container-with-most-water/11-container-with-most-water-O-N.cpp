9a4cc435371678d3a4eadf9cf535ddc7ff2691ed
tiankonguse <i@tiankonguse.com>
2021-06-06 17:02:29 +0800

    add 11. 盛最多水的容器 & Container With Most Water

---
+++ problemset-new/000/00011-container-with-most-water/11-container-with-most-water-O-N.cpp
@@ -0,0 +1,62 @@
+#include "base.h"
+
+typedef long long ll;
+typedef long long LL;
+typedef unsigned long long ull;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// const int mod = 1e9 + 7;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+// lower_bound 大于等于
+// upper_bound 大于
+// vector/array: upper_bound(vec.begin(), vec.end(), v)
+// map: m.upper_bound(v)
+// reserve vector预先分配内存
+// reverse(v.begin(), v.end()) 反转
+// sum = accumulate(a.begin(), a.end(), 0);
+// unordered_map / unordered_set
+// 排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+// 优先队列 priority_queue<Node>：大于是升序
+// struct Node{
+//     int t;
+//     bool operator<(const Node & that)const { return this->t > that.t; }
+// };
+
+const LL INF = 0x3f3f3f3f3f3f3f3fll;
+const double PI = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
+
+class Solution {
+ public:
+  int maxArea(vector<int>& height) {
+    int n = height.size();
+    int l = 0, r = n - 1;
+    int ans = 0;
+    while (l < r) {
+      if (height[l] < height[r]) {
+        ans = max(ans, height[l] * (r - l));
+        l++;
+      } else {
+        ans = max(ans, height[r] * (r - l));
+        r--;
+      }
+    }
+
+    return ans;
+  }
+};
+
+int main() {
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
