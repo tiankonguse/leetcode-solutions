7ba739fa2a5af8497db1f26ee922e87f64108db5
tiankonguse <i@tiankonguse.com>
2021-06-06 19:58:24 +0800

    add 19. 删除链表的倒数第 N 个结点 & Remove Nth Node From End of List

---
+++ problemset-new/000/00019-remove-nth-node-from-end-of-list/19-tiankonguse.cpp
@@ -0,0 +1,80 @@
+#include "base.h"
+
+typedef long long ll;
+typedef long long LL;
+typedef unsigned long long ull;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// const int mod = 1e9 + 7;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+// lower_bound 大于等于
+// upper_bound 大于
+// vector/array: upper_bound(vec.begin(), vec.end(), v)
+// map: m.upper_bound(v)
+// reserve vector预先分配内存
+// reverse(v.begin(), v.end()) 反转
+// sum = accumulate(a.begin(), a.end(), 0);
+// unordered_map / unordered_set
+// 排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+// 优先队列 priority_queue<Node>：大于是升序
+// struct Node{
+//     int t;
+//     bool operator<(const Node & that)const { return this->t > that.t; }
+// };
+
+const LL INF = 0x3f3f3f3f3f3f3f3fll;
+const double PI = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  ListNode* GetNext(ListNode* head, int n) {
+    while (--n) {
+      head = head->next;
+    }
+    return head;
+  }
+
+ public:
+  ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* tail = GetNext(head, n);
+    if (tail->next == nullptr) {  // 删除第一个
+      return head->next;
+    }
+
+    ListNode* root = head;
+    ListNode* pre = head;
+    while (tail->next != nullptr) {
+      pre = head;
+      head = head->next;
+      tail = tail->next;
+    }
+
+    pre->next = head->next;
+    return root;
+  }
+};
+
+int main() {
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
