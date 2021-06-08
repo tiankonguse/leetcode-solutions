976a7d54d8cbe6416392ec1b778992e7bfa1ae1e
tiankongever <50074856+tiankongever@users.noreply.github.com>
2021-06-08 23:49:36 +0800

    add 四数之和 & 4Sum

---
+++ problemset-new/000/00018-4sum/18-tiankonguser.cpp
@@ -0,0 +1,75 @@
+#include "base.h"
+
+typedef long long ll;
+typedef long long LL;
+typedef unsigned long long ull;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// const int mod = 1e9 + 7;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+// lower_bound 大于等于
+// upper_bound 大于
+// vector/array: upper_bound(vec.begin(), vec.end(), v)
+// map: m.upper_bound(v)
+// reserve vector预先分配内存
+// reverse(v.begin(), v.end()) 反转
+// sum = accumulate(a.begin(), a.end(), 0);
+// unordered_map / unordered_set
+// 排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+// 优先队列 priority_queue<Node>：大于是升序
+// struct Node{
+//     int t;
+//     bool operator<(const Node & that)const { return this->t > that.t; }
+// };
+
+const LL INF = 0x3f3f3f3f3f3f3f3fll;
+const double PI = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
+
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if (nums.size() < 4) return ans;
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 0; i + 3 < n; i++){
+            for(int j = i + 1; j + 2 < n;j++){
+                int l = j + 1, r = n - 1;
+                while(l < r){
+                    auto sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if(sum < target){
+                        l++;
+                    }else if(sum > target){
+                        r--;
+                    }else{
+                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        l++, r--;
+                    }
+                }
+            }
+        }
+
+        
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return ans;
+    }
+};
+
+int main() {
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
