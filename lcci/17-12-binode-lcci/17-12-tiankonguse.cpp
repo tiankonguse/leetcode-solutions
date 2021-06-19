93f73c139a1b99578a00425ffb68827f46ebbae1
tiankongever <50074856+tiankongever@users.noreply.github.com>
2021-06-20 00:46:31 +0800

    面试题 17.12. BiNode

---
+++ lcci/17-12-binode-lcci/17-12-tiankonguse.cpp
@@ -0,0 +1,85 @@
+#include "base.h"
+
+typedef long long ll;
+typedef long long LL;
+typedef unsigned long long ull;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// const int mod = 1e9 + 7;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+// int dir[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+// int dir[8][2] = {{0,1},{1, 1},{1,0},{1,-1}, {0,-1}, {-1, -1}, {-1,0}, {-1, 1}};
+// lower_bound 大于等于
+// upper_bound 大于
+// vector/array: upper_bound(vec.begin(), vec.end(), v)
+// map: m.upper_bound(v)
+// reserve vector预先分配内存
+// reverse(v.begin(), v.end()) 反转
+// sum = accumulate(a.begin(), a.end(), 0);
+// unordered_map / unordered_set
+// 排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+// 优先队列 priority_queue<Node>：大于是升序
+// struct Node{
+//     int t;
+//     bool operator<(const Node & that)const { return this->t > that.t; }
+// };
+
+const LL INF = 0x3f3f3f3f3f3f3f3fll;
+const double PI = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    pair<TreeNode*, TreeNode*> Dfs(TreeNode* root){
+        if(root == NULL) return {NULL, NULL};
+        
+        auto left = Dfs(root->left);
+        auto right = Dfs(root->right);
+
+        root->left = NULL;
+        root->right = NULL;
+
+        pair<TreeNode*, TreeNode*> ans = {root, root};
+
+        if(left.first != NULL){
+            ans.first = left.first;
+            left.second->right = root;
+        }
+
+        if(right.first != NULL){
+            ans.second = right.second;
+            root->right = right.first;
+        }
+
+        return ans;
+    }
+public:
+    TreeNode* convertBiNode(TreeNode* root) {
+        auto p = Dfs(root);
+        return p.first;
+    }
+};
+
+int main() {
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
