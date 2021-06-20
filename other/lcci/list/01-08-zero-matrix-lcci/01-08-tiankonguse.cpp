3f477cc58a12ce190382649363b6804c94dc8e8c
tiankonguse <i@tiankonguse.com>
2021-06-20 21:08:53 +0800

    面试题 01.08. 零矩阵

---
+++ other/lcci/list/01-08-zero-matrix-lcci/01-08-tiankonguse.cpp
@@ -0,0 +1,82 @@
+#include "base.h"
+
+typedef long long ll;
+typedef long long LL;
+typedef unsigned long long ull;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// const int mod = 1e9 + 7;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+// int dir[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+// int dir[8][2] = {{0,1},{1, 1},{1,0},{1,-1}, {0,-1}, {-1, -1}, {-1,0}, {-1, 1}};
+// lower_bound 大于等于
+// upper_bound 大于
+// vector/array: upper_bound(vec.begin(), vec.end(), v)
+// map: m.upper_bound(v)
+// reserve vector预先分配内存
+// reverse(v.begin(), v.end()) 反转
+// sum = accumulate(a.begin(), a.end(), 0);
+// unordered_map / unordered_set
+// 排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+// 优先队列 priority_queue<Node>：大于是升序
+// struct Node{
+//     int t;
+//     bool operator<(const Node & that)const { return this->t > that.t; }
+// };
+
+const LL INF = 0x3f3f3f3f3f3f3f3fll;
+const double PI = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
+
+
+class Solution {
+    int n, m;
+
+    void Fill(vector<vector<int>>& matrix, int x, int y) {
+        for(int i=0;i<n;i++){
+            matrix[i][y] = 0;
+        }
+        for(int i=0;i<m;i++){
+            matrix[x][i] = 0;
+        }
+    }
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        if(matrix.size() == 0) return;
+        if(matrix[0].size() == 0) return;
+
+        n = matrix.size();
+        m = matrix[0].size();
+
+        queue<pair<int, int>> que;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    que.push({i, j});
+                }
+            }
+        }
+
+        while(!que.empty()){
+            auto p = que.front();
+            que.pop();
+            Fill(matrix, p.first, p.second);
+        }
+
+    }
+};
+
+int main() {
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
