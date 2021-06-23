e6225fddf23331796b303f3981daaab9c1de7b57
tiankongever <50074856+tiankongever@users.noreply.github.com>
2021-06-23 23:23:26 +0800

    add 1114. 按序打印

---
+++ problemset-new/000/00031-next-permutation/31-tiankonguse.cpp
@@ -0,0 +1,67 @@
+#include "base.h"
+
+typedef long long ll;
+typedef long long LL;
+typedef unsigned long long ull;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+// const int mod = 1e9 + 7;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+// int dir[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+// int dir[8][2] = {{0,1},{1, 1},{1,0},{1,-1}, {0,-1}, {-1, -1}, {-1,0}, {-1, 1}};
+// lower_bound 大于等于
+// upper_bound 大于
+// vector/array: upper_bound(vec.begin(), vec.end(), v)
+// map: m.upper_bound(v)
+// reserve vector预先分配内存
+// reverse(v.begin(), v.end()) 反转
+// sum = accumulate(a.begin(), a.end(), 0);
+// unordered_map / unordered_set
+// 排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+// 优先队列 priority_queue<Node>：大于是升序
+// __builtin_popcount 快速得到 1 的个数
+// struct Node{
+//     int t;
+//     bool operator<(const Node & that)const { return this->t > that.t; }
+// };
+
+const LL INF = 0x3f3f3f3f3f3f3f3fll;
+const double PI = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2100, max4 = 11100, max5 = 200100, max6 = 2000100;
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]) {
+            i--;
+        }
+
+        if(i >= 0){
+            int j = n - 1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
+int main() {
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
