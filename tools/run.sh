3ff3123c2579ea65c62c62e2dc4e9ae337e92f0f
tiankonguse <i@tiankonguse.com>
2022-03-30 20:31:36 +0800

    move problem to new

--- tools/run.sh
+++ tools/run.sh
@@ -9,9 +9,29 @@
 echo "begin" 
 echo  "" > ../link.md;
 
+num=0
 for i in ../problemset/* ; do
-    i=`echo $i | sed 's/^.*\/\([^/]\+\)$/\1/'`;
-    echo "run $i"
-    echo "* $i [problem](./problemset/$i) [test](https://leetcode.com/problems/$i/) " >> ../link.md;  
+    #i=`echo $i | sed 's/^.*\/\([^/]\+\)$/\1/'`;
+    name=`echo $i | sed 's/..\/problemset\///'`;
+    newpath=`find ../problemset-new -iname "*[0-9]-$name"`
+    if [ "X"$newpath == "X" ];
+    then
+        echo "$name no find"
+        continue
+    fi
+    for j in $i/*; do
+        if [ $j == "$i/*" ]; then 
+            continue;
+        fi 
+        # echo "move $i $j"
+        mv $j $newpath/
+    done
+    rmdir $i
+    num=$(($num+1))
+    echo "num = $num"
+    # echo "* $i [problem](./problemset/$i) [test](https://leetcode.com/problems/$i/) " >> ../link.md;  
 done
 echo "end"
+
+
+# find . -iname "*-4sum" 
