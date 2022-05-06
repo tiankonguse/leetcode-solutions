68865eeb2f5153c7d895a87216bf3b6294379446
tiankonguse <i@tiankonguse.com>
2022-05-06 13:39:50 +0800

    add lcof-ii

--- tools/run-lcof-ii.sh
+++ tools/run-lcof-ii.sh
@@ -23,32 +23,40 @@ echo  "" >> $fineName
 data=$(curl -s "https://leetcode-cn.com/api/problems/all/" -m 3)
 
 echo "| ID | Title | Solutions |" >> $fineName
-echo "| :------: | :------: | :------: |" >> $fineName
+# echo "| :------: | :------: | :------: |" >> $fineName
 
 
 # linux: sed 's/"stat"/\n/g'
 # mac: sed -e 's/\"stat\"/\'$'\n/g'adv
 
 for line in $(echo $data | sed 's/ /___/g' | sed -e 's/\"stat\"/\'$'\n/g' | grep -E "question__title_slug" | grep "___Offer___II___"); do
-#    echo $line
+   echo $line
     echo "---"
     # {"question_id":1000228,"question__title":"\u6574\u6570\u9664\u6cd5","question__title_slug":"xoh6Oh","question__hide":false,"total_acs":33692,"total_submitted":160482,"total_column_articles":304,"frontend_question_id":"\u5251\u6307 Offer II 001"
 
     params=$(echo $line | sed 's/___/ /g' | sed 's/^.*"question__title":\s*"\([^"]*\)".*"question__title_slug":\s*"\([^"]*\)".*"frontend_question_id":\s*"\([^"]*\)".*$/\1___\2___\3/')
+    
+    echo $params
     question__title=$(echo $params | awk -F'___' '{ printf "%s",$1 }')
-    frontend_question_id=$(echo $params | awk -F'___' '{ printf "%s",$4 }')
+    # echo $question__title
+    frontend_question_id=$(echo $params | awk -F'___' '{ printf "%s",$3 }')
+    # echo $frontend_question_id
     question__title_slug=$(echo $params | awk -F'___' '{ printf "%s",$2 }')
-    pre=$(echo $frontend_question_id | sed 's/^.* Offer \(.*\)$/\1/' | sed 's/ //g' )
+    # echo $question__title_slug
+    pre=$(echo $frontend_question_id | sed 's/^.* Offer II \(.*\)$/\1/' | sed 's/ //g' )
     
     problem="$pre-$question__title_slug"
+    # echo $problem
+
     mkdir -p "../other/lcof-ii/list/$problem/"
+    touch ../other/lcof-ii/list/$problem/readme.md
 
     
     num=$(ls -l ../other/lcof-ii/list/$problem/ | grep -v "readme.md" | grep -v total |  wc -l)
 
     echo "run ${params}___${problem}"
     echo "${params}___${problem}___${pre}___${num}" | awk -F'___'  '{ printf "| 剑指 OFFER II %s | [题目](https://leetcode-cn.com/problems/%s/) | [题解数量（%d）](/other/lcof-ii/list/%s/) |\n",$5,$2,$6,$4 }' >> $fineName
-    # break
+    # # break
 done
 
 
