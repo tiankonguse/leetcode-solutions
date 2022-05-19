9c0a2db0a9878089d3d28f3d96a054eac52887c5
tiankonguse <i@tiankonguse.com>
2022-05-19 22:23:55 +0800

    add

---
+++ other/lcof-II/list/111-vlzXQL/111.cpp
@@ -0,0 +1,196 @@
+#include <bits/stdc++.h>
+
+#include "base.h"
+using namespace std;
+
+typedef __int128_t int128;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+
+typedef long double ld;
+typedef vector<ld> vld;
+
+typedef vector<bool> vb;
+typedef vector<string> vs;
+
+// const int mod = 1e9 + 7;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define rep1(i, n) for (ll i = 1; i <= (n); i++)
+#define rrep(i, n) for (ll i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (ll i = (n); i >= 1; i--)
+#define all(v) (v).begin(), (v).end()
+#define ALL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+#define sz(x) (int)(x).size()
+#define SZ(A) int((A).size())
+#define CPY(A, B) memcpy(A, B, sizeof(A))
+#define CTN(T, x) (T.find(x) != T.end())
+#define PB push_back
+#define MP(A, B) make_pair(A, B)
+#define fi first
+#define se second
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+int dir8[8][2] = {{0, 1},  {1, 1},   {1, 0},  {1, -1},
+                  {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+
+template <class T>
+inline void RST(T& A) {
+  memset(A, 0, sizeof(A));
+}
+template <class T>
+inline void FLC(T& A, int x) {
+  memset(A, x, sizeof(A));
+}
+template <class T>
+inline void CLR(T& A) {
+  A.clear();
+}
+template <class T>
+T& chmin(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = min(a, b);
+  }
+  return a;
+}
+template <class T>
+T& chmax(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = max(a, b);
+  }
+  return a;
+}
+
+constexpr int INF = 1 << 30;
+constexpr ll INFL = 1LL << 60;
+constexpr ll MOD = 1000000007;
+constexpr ld EPS = 1e-12;
+ld PI = acos(-1.0);
+
+const double pi = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2010, max4 = 20010, max5 = 200010, max6 = 2000010;
+// LONG_MIN, LONG_MAX
+
+/*
+unordered_map / unordered_set
+
+lower_bound 大于等于
+upper_bound 大于
+reserve 预先分配内存
+reverse(all(vec)) 反转
+sum = accumulate(a.begin(), a.end(), 0ll);
+__builtin_popcount 一的个数
+vector / array : upper_bound(all(vec), v)
+map: m.upper_bound(v)
+
+区间个数： std::distance(v.begin(), it)
+map/set distance 复杂度 O(N)
+vector/数组 distance 复杂度 O(1)
+
+
+
+size_t found=str.find(string/char/char*);
+std::string::npos
+
+排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+优先队列 priority_queue<Node>：top/pop/push/empty
+struct Node {
+  Node(int t = 0) : t(t) {}
+  int t;
+  // 小于是最大堆，大于是最小堆
+  bool operator<(const Node& that) const { return this->t < that.t; }
+};
+*/
+
+void floyd(vector<vector<double>>& g) {
+  int n = g.size();
+
+  rep(k, n) {
+    rep(i, n) {
+      if (i == k) continue;
+      rep(j, n) {
+        if (j == k || j == i) continue;
+        if (g[i][k] < 0 || g[k][j] < 0 || g[i][j] > 0) continue;
+        g[i][j] = g[i][k] * g[k][j];
+      }
+    }
+  }
+}
+
+class Solution {
+  map<string, int> m;
+  int index = 0;
+  void Add(string& s) {
+    if (m.count(s)) return;
+    m[s] = ++index;
+  }
+  int H(string& s) {
+    if (!m.count(s)) return 0;  // 0 代表不存在的节点
+    return m[s];
+  }
+  vector<vector<double>> g;
+
+ public:
+  vector<double> calcEquation(vector<vector<string>>& equations,
+                              vector<double>& values,
+                              vector<vector<string>>& queries) {
+    vector<double> ans;
+
+    for (auto& e : equations) {
+      rep(i, 2) {
+        Add(e[i]);
+        // printf("e[%d]=%s H=%d\n", i, e[i].c_str(), H(e[i]));
+      }
+    }
+    g.resize(index + 1, vector<double>(index + 1, -1.0));
+
+    rep(i, equations.size()) {
+      int a = H(equations[i][0]);
+      int b = H(equations[i][1]);
+      double c = values[i];
+      g[a][b] = c;
+      g[b][a] = 1 / c;
+      g[a][a] = 1;
+      g[b][b] = 1;
+    }
+
+    floyd(g);
+
+    for (auto& q : queries) {
+      int a = H(q[0]);
+      int b = H(q[1]);
+      ans.push_back(g[a][b]);
+    }
+
+    return ans;
+  }
+};
+
+int main() {
+  printf("hello ");
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
