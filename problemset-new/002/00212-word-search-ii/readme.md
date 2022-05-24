58de8047887f2094ffcf61d0d5c389fa660cdc35
tiankonguse <i@tiankonguse.com>
2022-05-24 20:21:22 +0800

    add

--- problemset-new/002/00212-word-search-ii/readme.md
+++ problemset-new/002/00212-word-search-ii/readme.md
@@ -1,1 +1,34 @@
-这道题还没有人做，你可以来抢占一血
+# 212. 单词搜索 II
+
+给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words， 返回所有二维网格上的单词 。
+
+单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+
+ 
+
+示例 1：
+
+
+输入：board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
+输出：["eat","oath"]
+示例 2：
+
+
+输入：board = [["a","b"],["c","d"]], words = ["abcb"]
+输出：[]
+ 
+
+提示：
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 12
+board[i][j] 是一个小写英文字母
+1 <= words.length <= 3 * 104
+1 <= words[i].length <= 10
+words[i] 由小写英文字母组成
+words 中的所有字符串互不相同
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/word-search-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
---
+++ problemset-new/002/00222-count-complete-tree-nodes/222-log-log-dfs.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+
+#include "base.h"
+using namespace std;
+
+typedef __int128_t int128;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+
+typedef long double ld;
+typedef vector<ld> vld;
+
+typedef vector<bool> vb;
+typedef vector<string> vs;
+
+// const int mod = 1e9 + 7;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define rep1(i, n) for (ll i = 1; i <= (n); i++)
+#define rrep(i, n) for (ll i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (ll i = (n); i >= 1; i--)
+#define all(v) (v).begin(), (v).end()
+#define ALL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+#define sz(x) (int)(x).size()
+#define SZ(A) int((A).size())
+#define CPY(A, B) memcpy(A, B, sizeof(A))
+#define CTN(T, x) (T.find(x) != T.end())
+#define PB push_back
+#define MP(A, B) make_pair(A, B)
+#define fi first
+#define se second
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+int dir8[8][2] = {{0, 1},  {1, 1},   {1, 0},  {1, -1},
+                  {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+
+template <class T>
+inline void RST(T& A) {
+  memset(A, 0, sizeof(A));
+}
+template <class T>
+inline void FLC(T& A, int x) {
+  memset(A, x, sizeof(A));
+}
+template <class T>
+inline void CLR(T& A) {
+  A.clear();
+}
+template <class T>
+T& chmin(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = min(a, b);
+  }
+  return a;
+}
+template <class T>
+T& chmax(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = max(a, b);
+  }
+  return a;
+}
+
+constexpr int INF = 1 << 30;
+constexpr ll INFL = 1LL << 60;
+constexpr ll MOD = 1000000007;
+constexpr ld EPS = 1e-12;
+ld PI = acos(-1.0);
+
+const double pi = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2010, max4 = 20010, max5 = 200010, max6 = 2000010;
+// LONG_MIN, LONG_MAX
+
+/*
+unordered_map / unordered_set
+
+lower_bound 大于等于
+upper_bound 大于
+reserve 预先分配内存
+reverse(all(vec)) 反转
+sum = accumulate(a.begin(), a.end(), 0ll);
+__builtin_popcount 一的个数
+vector / array : upper_bound(all(vec), v)
+map: m.upper_bound(v)
+
+区间个数： std::distance(v.begin(), it)
+map/set distance 复杂度 O(N)
+vector/数组 distance 复杂度 O(1)
+
+
+
+size_t found=str.find(string/char/char*);
+std::string::npos
+
+排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+优先队列 priority_queue<Node>：top/pop/push/empty
+struct Node {
+  Node(int t = 0) : t(t) {}
+  int t;
+  // 小于是最大堆，大于是最小堆
+  bool operator<(const Node& that) const { return this->t < that.t; }
+};
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+  int GetLeft(TreeNode* root) {
+    int h = 0;
+    while (root) {
+      h++;
+      root = root->left;
+    }
+    return h;
+  }
+  int GetRight(TreeNode* root) {
+    int h = 0;
+    while (root) {
+      h++;
+      root = root->right;
+    }
+    return h;
+  }
+
+ public:
+  int countNodes(TreeNode* root) {
+    if (root == nullptr) return 0;
+    if (root->left == nullptr) return 1;
+    int leftLeft = GetLeft(root);
+    int leftRight = GetRight(root->left);
+
+    int ans = 1;
+    if (leftLeft == leftRight + 1) {  // 左子树是满的
+      ans += (1 << leftRight) - 1;
+      ans += countNodes(root->right);
+    } else {  // 左子树非满
+      ans += (1 << leftRight) - 1;
+      ans += countNodes(root->left);
+    }
+    return ans;
+  }
+};
+
+int main() {
+  printf("hello ");
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
