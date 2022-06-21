f5705151e09fb7c41ea51b015cbdb0359c039dd4
tiankonguse <i@tiankonguse.com>
2022-06-21 23:12:21 +0800

    add

---
+++ other/lcci/list/17-22-word-transformer-lcci/17-22.cpp
@@ -0,0 +1,221 @@
+#include <bits/stdc++.h>
+
+#include "base.h"
+using namespace std;
+
+typedef __int128_t int128;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+
+typedef long double ld;
+typedef vector<ld> vld;
+
+typedef vector<bool> vb;
+typedef vector<string> vs;
+
+// const int mod = 1e9 + 7;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define rep1(i, n) for (ll i = 1; i <= (n); i++)
+#define rrep(i, n) for (ll i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (ll i = (n); i >= 1; i--)
+#define all(v) (v).begin(), (v).end()
+#define ALL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+#define sz(x) (int)(x).size()
+#define SZ(A) int((A).size())
+#define CPY(A, B) memcpy(A, B, sizeof(A))
+#define CTN(T, x) (T.find(x) != T.end())
+#define PB push_back
+#define MP(A, B) make_pair(A, B)
+#define fi first
+#define se second
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+int dir8[8][2] = {{0, 1},  {1, 1},   {1, 0},  {1, -1},
+                  {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+
+template <class T>
+inline void RST(T& A) {
+  memset(A, 0, sizeof(A));
+}
+template <class T>
+inline void FLC(T& A, int x) {
+  memset(A, x, sizeof(A));
+}
+template <class T>
+inline void CLR(T& A) {
+  A.clear();
+}
+template <class T>
+T& chmin(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = min(a, b);
+  }
+  return a;
+}
+template <class T>
+T& chmax(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = max(a, b);
+  }
+  return a;
+}
+
+constexpr int INF = 1 << 30;
+constexpr ll INFL = 1LL << 60;
+constexpr ll MOD = 1000000007;
+constexpr ld EPS = 1e-12;
+ld PI = acos(-1.0);
+
+const double pi = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2010, max4 = 20010, max5 = 200010, max6 = 2000010;
+// LONG_MIN, LONG_MAX
+
+/*
+unordered_map / unordered_set
+
+lower_bound 大于等于
+upper_bound 大于
+reserve 预先分配内存
+reverse(all(vec)) 反转
+sum = accumulate(a.begin(), a.end(), 0ll);
+__builtin_popcount 一的个数
+vector / array : upper_bound(all(vec), v)
+map: m.upper_bound(v)
+
+区间个数： std::distance(v.begin(), it)
+map/set distance 复杂度 O(N)
+vector/数组 distance 复杂度 O(1)
+
+vector 去重
+sort(nums.begin(), nums.end());
+nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+size_t found=str.find(string/char/char*);
+std::string::npos
+
+排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+优先队列 priority_queue<Node>：top/pop/push/empty
+struct Node {
+  Node(int t = 0) : t(t) {}
+  int t;
+  // 小于是最大堆，大于是最小堆
+  bool operator<(const Node& that) const { return this->t < that.t; }
+};
+
+srand((unsigned)time(NULL));
+rand();
+
+mt19937 gen{random_device{}()};
+uniform_real_distribution<double> dis(min, max);
+function<double(void)> Rand = [that = this]() { return that->dis(that->gen); };
+
+*/
+class Solution {
+  unordered_map<string, int> m;
+  vector<int> pre;
+  queue<int> que;
+  int n;
+
+  void Solver(vector<string>& wordList, int S, int E) {
+    que.push(S);
+    pre[S] = wordList.size();
+
+    while (!que.empty()) {
+      int i = que.front();
+      que.pop();
+      auto& s = wordList[i];
+      int from = m[s];
+
+      for (int j = 0; j < s.size(); j++) {
+        char base = s[j];
+        for (char c = 'a'; c <= 'z'; c++) {
+          if (c == base) continue;
+          s[j] = c;
+          if (m.count(s) == 0) continue;
+
+          int to = m[s];
+          if (pre[to] != -1) continue;
+
+          pre[to] = from;
+          que.push(to);
+          if (to == E) {
+            s[j] = base;
+            return;
+          }
+        }
+        s[j] = base;
+      }
+    }
+  }
+
+ public:
+  vector<string> findLadders(string beginWord, string endWord,
+                             vector<string>& wordList) {
+    for (auto& s : wordList) {
+      m[s] = -1;
+    }
+    if (m.count(endWord) == 0) return {};
+    if (beginWord == endWord) return {beginWord};
+
+    if (m.count(beginWord) == 0) {
+      m[beginWord] = -1;
+      wordList.push_back(beginWord);
+    }
+
+    n = wordList.size();
+    for (int i = 0; i < n; i++) {
+      auto& s = wordList[i];
+      m[s] = i;
+    }
+
+    for (auto& [s, p] : m) {
+      // printf("s=%s p=%d\n", s.data(), p);
+    }
+
+    pre.resize(n, -1);
+
+    Solver(wordList, m[endWord], m[beginWord]);
+
+    if (pre[m[beginWord]] == -1) return {};
+
+    vector<string> ans;
+
+    int p = m[beginWord];
+    while (p != n) {
+      ans.push_back(wordList[p]);
+      p = pre[p];
+    }
+    return ans;
+  }
+};
+
+int main() {
+  printf("hello ");
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
--- other/lcci/list/17-22-word-transformer-lcci/readme.md
+++ other/lcci/list/17-22-word-transformer-lcci/readme.md
@@ -1,1 +1,29 @@
-这道题还没有人做，你可以来抢占一血
+# 面试题 17.22. 单词转换
+
+给定字典中的两个词，长度相等。写一个方法，把一个词转换成另一个词， 但是一次只能改变一个字符。每一步得到的新词都必须能在字典中找到。
+
+编写一个程序，返回一个可能的转换序列。如有多个可能的转换序列，你可以返回任何一个。
+
+示例 1:
+
+输入:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+输出:
+["hit","hot","dot","lot","log","cog"]
+示例 2:
+
+输入:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+输出: []
+
+解释: endWord "cog" 不在字典中，所以不存在符合要求的转换序列。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/word-transformer-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
