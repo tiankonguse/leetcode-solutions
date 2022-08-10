905ddd745b62a016e1fcb6fb98800553e8169d98
tiankonguse <i@tiankonguse.com>
2022-08-10 22:06:35 +0800

    add

---
+++ problemset-new/006/00640-solve-the-equation/640.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+
+#include "base.h"
+using namespace std;
+
+typedef __int128_t int128;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+
+typedef long double ld;
+typedef vector<ld> vld;
+
+typedef vector<bool> vb;
+typedef vector<string> vs;
+
+// const int mod = 1e9 + 7;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define rep1(i, n) for (ll i = 1; i <= (n); i++)
+#define rrep(i, n) for (ll i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (ll i = (n); i >= 1; i--)
+#define all(v) (v).begin(), (v).end()
+#define ALL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+#define sz(x) (int)(x).size()
+#define SZ(A) int((A).size())
+#define CPY(A, B) memcpy(A, B, sizeof(A))
+#define CTN(T, x) (T.find(x) != T.end())
+#define PB push_back
+#define MP(A, B) make_pair(A, B)
+#define fi first
+#define se second
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+int dir4[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+int dir8[8][2] = {{0, 1},  {1, 1},   {1, 0},  {1, -1},
+                  {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+
+template <class T>
+inline void RST(T& A) {
+  memset(A, 0, sizeof(A));
+}
+template <class T>
+inline void FLC(T& A, int x) {
+  memset(A, x, sizeof(A));
+}
+template <class T>
+inline void CLR(T& A) {
+  A.clear();
+}
+template <class T>
+T& chmin(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = min(a, b);
+  }
+  return a;
+}
+template <class T>
+T& chmax(T& a, T b) {
+  if (a == -1) {
+    a = b;
+  } else {
+    a = max(a, b);
+  }
+  return a;
+}
+
+constexpr int INF = 1 << 30;
+constexpr ll INFL = 1LL << 60;
+constexpr ll MOD = 1000000007;
+constexpr ld EPS = 1e-12;
+ld PI = acos(-1.0);
+
+const double pi = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2010, max4 = 20010, max5 = 200010, max6 = 2000010;
+// LONG_MIN(10进制 10位), LONG_MAX(10进制 19位)
+
+/*
+unordered_map / unordered_set
+
+lower_bound 大于等于
+upper_bound 大于
+reserve 预先分配内存
+reverse(all(vec)) 反转
+sum = accumulate(a.begin(), a.end(), 0ll);
+__builtin_popcount 一的个数
+vector / array : upper_bound(all(vec), v)
+map: m.upper_bound(v)
+
+区间个数： std::distance(v.begin(), it)
+map/set distance 复杂度 O(N)
+vector/数组 distance 复杂度 O(1)
+
+vector 去重
+sort(nums.begin(), nums.end());
+nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+size_t found=str.find(string/char/char*);
+std::string::npos
+
+排序，小于是升序：[](auto&a, auto&b){ return a < b; })
+优先队列 priority_queue<Node>：top/pop/push/empty
+struct Node {
+  Node(int t = 0) : t(t) {}
+  int t;
+  // 小于是最大堆，大于是最小堆
+  bool operator<(const Node& that) const { return this->t < that.t; }
+};
+
+srand((unsigned)time(NULL));
+rand();
+
+mt19937 gen{random_device{}()};
+uniform_real_distribution<double> dis(min, max);
+function<double(void)> Rand = [that = this]() { return that->dis(that->gen); };
+
+*/
+
+class Solution {
+  string str;
+
+  int ReadSign(int& l, int r) {
+    char c = str[l];
+    int sign = 1;
+    if (c == '-') {
+      sign = -1;
+      l++;
+    } else if (c == '+') {
+      sign = 1;
+      l++;
+    }
+    return sign;
+  }
+
+  int ReadNum(int& l, int r) {
+    int val = 0;
+    while (l < r && str[l] >= '0' && str[l] <= 'r') {
+      val = val * 10 + (str[l] - '0');
+      l++;
+    }
+    return val;
+  }
+
+  pair<int, int> Parse(int l, int r) {
+    int a = 0, b = 0;
+
+    while (l < r) {
+      int sign = ReadSign(l, r);
+      if (str[l] == 'x') {
+        a += sign;
+        l++;
+      } else {
+        int val = ReadNum(l, r);
+        if (l < r && str[l] == 'x') {
+          a += sign * val;
+          l++;
+        } else {
+          b += sign * val;
+        }
+      }
+    }
+
+    return {a, b};
+  }
+
+ public:
+  string solveEquation(string equation_) {
+    str.swap(equation_);
+    int n = str.size();
+    auto mid = str.find('=');
+    auto [a0, b0] = Parse(0, mid);
+    auto [a1, b1] = Parse(mid + 1, n);
+
+    int a = a0 - a1;
+    int b = b0 - b1;
+
+    if (a == 0) {
+      if (b == 0) {
+        return "Infinite solutions";
+      } else {
+        return "No solution";
+      }
+    } else {
+      int c = -b / a;
+      return string("x=") + to_string(c);
+    }
+  }
+};
+
+int main() {
+  printf("hello ");
+  //   vector<double> ans = {1.00000,-1.00000,3.00000,-1.00000};
+  //   vector<vector<int>> cars = {{1, 2}, {2, 1}, {4, 3}, {7, 2}};
+  //   TEST_SMP1(Solution, getCollisionTimes, ans, cars);
+
+  return 0;
+}
--- problemset-new/006/00640-solve-the-equation/readme.md
+++ problemset-new/006/00640-solve-the-equation/readme.md
@@ -1,1 +1,33 @@
-这道题还没有人做，你可以来抢占一血
+# 640. 求解方程
+
+求解一个给定的方程，将x以字符串 "x=#value" 的形式返回。该方程仅包含 '+' ， '-' 操作，变量 x 和其对应系数。
+
+如果方程没有解，请返回 "No solution" 。如果方程有无限解，则返回 “Infinite solutions” 。
+
+题目保证，如果方程中只有一个解，则 'x' 的值是一个整数。
+
+ 
+
+示例 1：
+
+输入: equation = "x+5-3+x=6+x-2"
+输出: "x=2"
+示例 2:
+
+输入: equation = "x=x"
+输出: "Infinite solutions"
+示例 3:
+
+输入: equation = "2x=x"
+输出: "x=0"
+ 
+
+提示:
+
+3 <= equation.length <= 1000
+equation 只有一个 '='.
+equation 方程由整数组成，其绝对值在 [0, 100] 范围内，不含前导零和变量 'x' 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/solve-the-equation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
