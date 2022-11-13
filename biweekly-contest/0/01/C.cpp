3e5716a36f2fa522ba0df78e443146fa8c9cf239
tiankonguse <i@tiankonguse.com>
2022-11-13 14:05:39 +0800

    add

---
+++ biweekly-contest/0/01/C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include "base.h"
+using namespace std;
+
+
+// 该题目是 Plus 会员专享题
+typedef long long ll;
+class Solution {
+ public:
+  int minJump(vector<int>& jump) {
+    int n = jump.size();
+
+    return 0;
+  }
+};
---
+++ include/smp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include "base.h"
+using namespace std;
+
+
+typedef long long ll;
+class Solution {
+ public:
+  int minJump(vector<int>& jump) {
+    int n = jump.size();
+
+    return 0;
+  }
+};
