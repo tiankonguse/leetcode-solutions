516a7f0f4538083dd3c0dda9a8de1901652a228b
tiankonguse <i@tiankonguse.com>
2024-09-30 16:59:10 +0800

    add hpome mac comnile and run enviriment

---
+++ other/CSP-J/2023/bus/bus.cpp
@@ -0,0 +1,85 @@
+/*
+ID: tiankonguse
+TASK: bus
+LANG: C++
+*/
+#define TASK "bus"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+constexpr int INF = 1 << 30;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+void InitIO() {
+// #ifndef USACO_LOCAL_JUDGE
+//   freopen(TASK ".in", "r", stdin);
+//   freopen(TASK ".out", "w", stdout);
+// #endif
+}
+
+vector<vector<pair<ll, ll>>> g;  // g[u]{v, a}
+min_queue<pair<ll, ll>> que;     // {cost, u}
+vector<vector<ll>> dp;
+
+
+/*
+优先队列求最短路，每个节点最多可以到达k次
+*/
+void Solver() {  //
+  ll n, m, k;
+  scanf("%lld%lld%lld", &n, &m, &k);
+  g.resize(n);
+  dp.resize(k, vector<ll>(n, INF));
+
+  while (m--) {
+    ll u, v, a;
+    scanf("%lld%lld%lld", &u, &v, &a);  // u->v
+    u--, v--;                           // 下标转换为从0开始
+    g[u].push_back({v, a});
+  }
+
+  dp[0][0] = 0;
+  que.push({0, 0});
+
+  while (!que.empty()) {
+    auto [tu, u] = que.top();
+    que.pop();
+
+    // 剪枝，如果有更优解，说明已经计算过了
+    if (dp[tu % k][u] < tu) {
+      continue;
+    }
+
+    for (auto [v, a] : g[u]) {
+      ll tv = tu;
+      if (tv < a) {  // 道路没开放，在门口等到 num 个 k
+        ll num = (a - tv + k - 1) / k;
+        tv += num * k;
+      }
+      tv++;  // 通过这条道路，时间加1
+
+      ll tvk = tv % k;
+      if (tv < dp[tvk][v]) {  // 有更优解
+        dp[tvk][v] = tv;
+        que.push({tv, v});
+      }
+    }
+  }
+
+  if (dp[0][n - 1] == INF) {
+    dp[0][n - 1] = -1;
+  }
+  printf("%lld\n", dp[0][n - 1]);
+}
+
+int main() {
+  InitIO();
+  Solver();
+  return 0;
+}
--- readme.md
+++ readme.md
@@ -30,3 +30,32 @@
 * .gitignore : 项目配置文件，保留。    
 * [join.md](join.md) 加入项目手册  
 
+## vscode
+
+
+```
+gcc -v
+clang -v
+brew install gcc
+
+gcc-13 -v
+
+
+macOS 的 debugger 是 lldb,，得自己安装gdb
+brew install gdb
+whereis gdb
+WARNING: Debugger executable '/usr/local/bin/gdb' is not signed. As a result, debugging may not work properly.
+ERROR: Unable to start debugging. Unexpected GDB output from command "-exec-run". Unable to find Mach task port for process-id 91593: (os/kern) failure (0x5).
+ (please check gdb is codesigned - see taskgated(8))
+
+1. 打开 Keychain Access
+2. 菜单项Keychain Access --> Certificate Assistant --> Create a Certificate... ，点击Create a Certificate... 
+3. 给证书选个名字，此处是 gdb_codesign，可以用其他名字; 设置 Identity Type 为 Self Signed Root ; 设置 Certificate Type 为 Code Signing ; 勾选 Let me override defaults，然后Continue
+4. 接下来默认即可（中间截图省略），点击几次Continue，直到为证书指定位置（Specify a Location For The Certificate），设置 Keychain 为 System ，然后 Create，输入密码
+
+确保 keychain 是 System keychain
+security find-certificate -c gdb_codesign |grep System.keychain
+确保证书没有过期
+security find-certificate -p -c gdb_codesign | openssl x509 -checkend 0
+
+```
