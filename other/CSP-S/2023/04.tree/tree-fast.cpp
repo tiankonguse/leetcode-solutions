40f456e62cca787b2acc8c55df642d68b53c6414
tiankonguse <i@tiankonguse.com>
2025-06-11 19:52:25 +0800

    add

--- other/CSP-S/2023/04.tree/tree-fast.cpp
+++ other/CSP-S/2023/04.tree/tree-fast.cpp
@@ -15,17 +15,28 @@ using namespace std;
 typedef long long ll;
 using int128 = __int128;
 
-ll debug = 0;
-#define myprintf(...)                        \
-  do {                                       \
-    if (debug) fprintf(stdout, __VA_ARGS__); \
+#ifdef USACO_LOCAL_JUDGE
+int debug_log = 0;
+int debug_assert = 1;
+#define MyPrintf(...)                   \
+  do {                                  \
+    if (debug_log) printf(__VA_ARGS__); \
   } while (0)
+
+#define MyAssert(...)                      \
+  do {                                     \
+    if (debug_assert) assert(__VA_ARGS__); \
+  } while (0)
+#else
+#define MyPrintf(...)
+#define MyAssert(...)
+#endif
 
 void InitIO() {
 #ifdef USACO_LOCAL_JUDGE
-#define TASKNO "2"
-  // freopen(TASK TASKNO ".in", "r", stdin);
-  // freopen(TASK TASKNO ".out", "w", stdout);
+#define TASKNO "1"
+  freopen(TASK TASKNO ".in", "r", stdin);
+  freopen(TASK TASKNO ".out", "w", stdout);
 #endif
 }
 ll rd() {
@@ -47,7 +58,7 @@ vector<vector<ll>> g;
 ll n;
 
 vector<ll> lastDay;
-// vector<ll> minDay;  // 最少需要多少天
+vector<ll> father;
 
 /*
 h=max(b+xc,1)
@@ -62,29 +73,29 @@ b - 1 >= -cx
 */
 
 int128 mySqrt(int128 A) {
-  return sqrt(A);
-  // int128 l = sqrt(A);
-  // int128 r = l * 2;
-  // while (l < r) {
-  //   int128 mid = (l + r) / 2;
-  //   int128 mm = mid * mid;
-  //   if (mm >= A) {
-  //     r = mid;
-  //   } else {
-  //     l = mid + 1;
-  //   }
-  // }
-  // if (r * r > A) {
-  //   r--;
-  // }
-  // return r;
+  // return sqrt(A);
+  int128 l = sqrt((long double)A);
+  int128 r = l * 2;
+  while (l < r) {
+    int128 mid = (l + r) / 2;
+    int128 mm = mid * mid;
+    if (mm >= A) {
+      r = mid;
+    } else {
+      l = mid + 1;
+    }
+  }
+  if (r * r > A) {
+    r--;
+  }
+  return r;
 }
 
 // 判断 maxDay 天内，所有节点不考虑依赖，是否可以满足要求
 bool CheckDownDay(int128 maxDay, int128 A, int128 B, int128 C, ll& ret) {
   int128 Y = maxDay;
   int128 lastVal = B - Y * C;
-  assert(lastVal > 0);
+  MyAssert(lastVal > 0);
   int128 allHeight = ((B - C) + (B - Y * C)) * Y / 2;
   if (allHeight < A) {
     return false;
@@ -96,22 +107,23 @@ bool CheckDownDay(int128 maxDay, int128 A, int128 B, int128 C, ll& ret) {
     ret = maxDay;
     return true;
   }
+  // (B - C*X + B - C*Y) * (Y - X + 1) >= 2*A
+  // (2*B - C*Y - C*X) * (Y + 1 - X) >= 2*A
+  // (2*B - C*Y) * (Y + 1) - (2*B - C*Y) * X - (Y+1) * C * X + C * X^2 >= 2*A
+  // (2*B - C*Y) * (Y + 1) - 2*B*X + C*Y*X - Y*C*X - C*X + C*X^2 >= 2*A
+  // (2*B - C*Y) * (Y + 1) - 2*B*X - C*X + C*X^2 >= 2*A
+  // C*X^2 - (2*B + C) * X + (2*B - C*Y) * (Y + 1) - 2*A >= 0
   int128 AA = C;
   int128 BB = -(2 * B + C);
-  int128 CC = (2 * B - Y * C) * (Y + 1) - 2 * A;
+  int128 CC = (2 * B - C * Y) * (Y + 1) - 2 * A;
   int128 X = (-BB - mySqrt(BB * BB - 4 * AA * CC)) / (2 * AA);
-  // k = -BB/(2*AA) = (2 * B + C)/(2*C) = B/C + 1/2
-  assert(X < maxDay);
-  // if (X < maxDay) {
-  //   X++;
-  // }
-  // if (X > maxDay) {
-  //   X--;
-  // }
-  assert((B + X * C + B + Y * C) * (Y - X + 1) / 2 >= A);
-  // while ((B - X * C + B - Y * C) * (Y - X + 1) / 2 < A) {
-  //   X--;
-  // }
+  // k = -BB/(2*AA) = (2 * B + C)/(2*C) = B/C + 1/2, 取左边的，x 不能向下取整
+  MyAssert(X < maxDay);
+  // 如果不是整数解，答案会向下取整，需要减一位
+  if ((B - X * C + B - Y * C) * (Y - X + 1) / 2 < A) {
+    X--;
+  }
+  MyAssert((B - X * C + B - Y * C) * (Y - X + 1) / 2 >= A);
   ret = X;
   return true;
 }
@@ -142,15 +154,15 @@ bool CheckLastDay(const int128 maxDay, const int128 A, const int128 B, int128 C,
     // if (X < maxDay) {
     //   X++;
     // }
-    assert(X < maxDay);
-    assert((B + X * C + B + Y * C) * (Y - X + 1) / 2 >= A);
+    MyAssert(X < maxDay);
+    MyAssert((B + X * C + B + Y * C) * (Y - X + 1) / 2 >= A);
     // while ((B + X * C + B + Y * C) * (Y - X + 1) / 2 < A) {
     //   X--;
     // }
     ret = X;
     return true;
   } else if (C == 0) {
-    ll minDay = (A + 1) / B;
+    ll minDay = (A + B - 1) / B;
     if (minDay > maxDay) {
       return false;
     }
@@ -172,24 +184,80 @@ bool CheckLastDay(const int128 maxDay, const int128 A, const int128 B, int128 C,
     }
   }
 }
-ll Dfs(int u, int pre) {
-  for (auto v : g[u]) {
-    if (v == pre) continue;
-    lastDay[u] = min(lastDay[u], Dfs(v, u) - 1);
-  }
-  return lastDay[u];
+
+inline bool FixRangeSum(const ll i, const ll x0, const ll xn, ll A) {
+  const auto [a, b, c] = points[i];
+  const int128 down = xn - x0 + 1;
+  const int128 low = min(x0 * c + b, xn * c + b);
+  const int128 high = max(x0 * c + b, xn * c + b);
+  return (high + low) * down / 2 >= A;
 }
+bool CheckRangeSum(const ll i, const ll x0, const ll xn) {
+  const auto [a, b, c] = points[i];
+  ll A = a;
+  if (c >= 0) {
+    return FixRangeSum(i, x0, xn, a);
+  }
+  // c < 0
+  const ll one = (b - 1) / (-c);
+  if (x0 > one) {  // 全是 1
+    return xn - x0 + 1 >= A;
+  } else if (xn <= one) {  // 斜线
+    return FixRangeSum(i, x0, xn, a);
+  } else {  // 一半斜线 + 一半 1
+    if (xn - one >= A) {
+      return true;
+    }
+    return FixRangeSum(i, x0, one, a - (xn - one));
+  }
+}
+ll Cal(const ll i, const ll d) {  //
+  // const auto [a, b, c] = points[i];
+  ll l = 1, r = d + 1;
+  while (l < r) {                // [l, r)
+    const ll mid = (l + r) / 2;  // sum(mid, d)
+    if (CheckRangeSum(i, mid, d)) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
+  if (r == 1) {  // 不满足要求
+    return -1;
+  }
+  return r - 1;
+}
+
+inline ll CalFast(const ll i, const ll maxDay) {
+  auto [a, b, c] = points[i];
+  ll ret = 0;
+  if (!CheckLastDay(maxDay, a, b, c, ret)) {
+    return -1;
+  }
+  return ret;
+}
+
 bool Check(ll maxDay) {
   // 第一步，计算每棵树最晚在哪天种植
   for (ll i = 0; i < n; i++) {
-    auto [a, b, c] = points[i];
-    if (!CheckLastDay(maxDay, a, b, c, lastDay[i])) {
+    // auto [a, b, c] = points[i];
+    // ll acRet = Cal(i, maxDay);
+    ll fastRet = CalFast(i, maxDay);
+    // assert(acRet == fastRet);
+    // MyPrintf("a=%lld b=%lld c=%lld\n", a, b, c);
+    lastDay[i] = fastRet;
+    if (lastDay[i] == -1) {
       return false;
     }
   }
   // 第二步：递归计算出树，修正 lastDay，父亲的 lastDay 需要小于儿子的
-  if (Dfs(0, -1) < 1) {
-    return false;
+
+  for (int i = 0; i < n; i++) {
+    if (lastDay[i] < 1) return false;
+    int pre = father[i];
+    if (pre != -1) {
+      lastDay[pre] = min(lastDay[pre], lastDay[i] - 1);
+    }
   }
   // 第三步：排序
   sort(lastDay.begin(), lastDay.end());
@@ -201,38 +269,22 @@ bool Check(ll maxDay) {
   }
   return true;
 }
+void Dfs(int u, int pre) {
+  for (auto v : g[u]) {
+    if (v == pre) continue;
+    Dfs(v, u);
+  }
+  father[u] = pre;
+}
 
-// ll SolverMinDay(ll a, ll b, ll c) {
-//   if (c > 0) {
-//     int128 AA = c;
-//     int128 BB = 2 * b + c;
-//     int128 CC = -2 * a;
-//     int128 X = (sqrt(BB * BB - 4 * AA * CC) - BB) / (2 * AA);
-//     if ((b + c + b + X * c) * X / 2 < a) {
-//       X++;
-//     }
-//     return X;
-//   } else if (c == 0) {
-//     return (a + b - 1) / b;
-//   } else {
-//     c = -c;
-//     int128 oneDay = (b - 1) / c;
-//     int128 Y = (b - c + b + oneDay * c) * oneDay / 2;
-//     if (Y < a) {
-//       return oneDay + a - Y;  // 差的使用 1 补上
-//     } else if (Y == a) {
-//       return oneDay;  // 恰好够
-//     } else {
-
-//     }
-//   }
-// }
 void Solver() {  //
+  // ll ret = 0;
+  // CheckDownDay(4, 7, 10, 2, ret);
   // scanf("%lld", &n);
   n = rd();
   points.reserve(n);
   lastDay.resize(n);
-  // minDay.resize(n);
+  father.resize(n);
   g.resize(n);
   // myprintf("n=%lld\n", n);
   for (ll i = 0; i < n; i++) {
@@ -245,7 +297,6 @@ void Solver() {  //
       c = 0;
     }
     points.push_back({a, b, c});
-    // minDay[i] = SolverMinDay(a, b, c);
   }
   for (ll i = 1; i < n; i++) {
     ll u, v;
@@ -256,6 +307,7 @@ void Solver() {  //
     g[u].push_back(v);
     g[v].push_back(u);
   }
+  Dfs(0, -1);
 
   ll l = n, r = 10e9 + 1;
   while (l < r) {  //[l,r)
