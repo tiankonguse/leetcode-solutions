6a2af3e26f9c0d6fddd0422477e18963bf093395
tiankonguse <i@tiankonguse.com>
2025-09-25 22:05:39 +0800

    add

---
+++ other/CSP-J/2019/1.number/number.cpp
@@ -0,0 +1,115 @@
+/*
+ID: tiankonguse
+TASK: number
+LANG: C++
+MAC EOF: ctrl+D
+link:  https://www.luogu.com.cn/problem/P5660
+PATH:  P5660 [CSP-J2019] 数字游戏
+submission:
+*/
+#define TASK "number"
+#define TASKEX ""
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void CheckUsacoTask() {
+#ifdef USACO_LOCAL_JUDGE
+  string filePath = __FILE__;
+  string fileNameEx = filePath.substr(filePath.rfind('/') + 1);
+  string fileName = fileNameEx.substr(0, fileNameEx.find("."));
+  assert(fileName == TASK TASKEX);
+#endif
+}
+
+#ifdef USACO_LOCAL_JUDGE
+int debug_log = 0;
+int debug_assert = 0;
+#define MyPrintf(...)                   \
+  do {                                  \
+    if (debug_log) printf(__VA_ARGS__); \
+  } while (0)
+
+#define MyAssert(...)                      \
+  do {                                     \
+    if (debug_assert) assert(__VA_ARGS__); \
+  } while (0)
+#else
+#define MyPrintf(...)
+#define MyAssert(...)
+#endif
+
+constexpr int INF = 1 << 30;
+constexpr ll INFL = 1LL << 60;
+constexpr ll MOD = 1000000007;
+
+const double pi = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2010, max4 = 20010, max5 = 200010, max6 = 2000010;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+void InitIO(int fileIndex) {  //
+// #ifdef USACO_LOCAL_JUDGE
+// #define USACO_TASK_FILE 3
+// #define TASKNO 1
+// #ifndef USACO_TASK_FILE
+//   fileIndex = TASKNO;
+// #endif
+//   string fileInName = string(TASK) + to_string(fileIndex) + ".in";
+//   string fileOutName = string(TASK) + to_string(fileIndex) + ".out";
+//   freopen(fileInName.c_str(), "r", stdin);
+//   freopen(fileOutName.c_str(), "w", stdout);
+// #endif
+}
+
+void Solver() {  //
+  char str[10];
+  scanf("%s", str);
+  int len = strlen(str);
+  int ans = 0;
+  for (int i = 0; i < len; i++) {
+    ans += str[i] - '0';
+  }
+  printf("%d\n", ans);
+}
+
+void ExSolver() {
+#ifdef USACO_LOCAL_JUDGE
+#ifdef USACO_TASK_FILE
+  auto t1 = std::chrono::steady_clock::now();
+#endif
+#endif
+  Solver();
+#ifdef USACO_LOCAL_JUDGE
+#ifdef USACO_TASK_FILE
+  auto t2 = std::chrono::steady_clock::now();
+  auto my = std::chrono::duration_cast<std::chrono::duration<double, ratio<1, 1000>>>(t2 - t1);
+  printf("my 用时: %.0lfms\n", my.count());
+#endif
+#endif
+}
+
+int main(int argc, char** argv) {
+  CheckUsacoTask();
+  int fileIndex = 1;
+#ifdef USACO_TASK_FILE
+  for (int i = 1; i <= USACO_TASK_FILE; i++) {
+    fileIndex = i;
+#endif
+    InitIO(fileIndex);
+    ExSolver();
+#ifdef USACO_TASK_FILE
+  }
+#endif
+  return 0;
+}
+
+/*
+
+*/
