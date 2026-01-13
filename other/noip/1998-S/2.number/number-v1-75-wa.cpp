3e49751d59a6252ae8087bbf18d2d2f20102460f
tiankonguse <i@tiankonguse.com>
2026-01-14 01:20:29 +0800

    add

---
+++ other/noip/1998-S/2.number/number-v1-75-wa.cpp
@@ -0,0 +1,197 @@
+/*
+ID: tiankonguse
+TASK: number
+LANG: C++
+MAC EOF: ctrl+D
+link:
+PATH:
+submission:
+*/
+#define TASK "number"
+#define TASKEX ""
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void CheckUsacoTask() {
+#ifdef USACO_LOCAL_JUDGE
+  // 获取当前文件的完整路径
+  string filePath = __FILE__;
+  // 从路径中提取文件名（包含扩展名）
+  string fileNameEx = filePath.substr(filePath.rfind('/') + 1);
+  // 提取文件名（不包含扩展名）
+  string fileName = fileNameEx.substr(0, fileNameEx.find("."));
+  // 检查文件名是否与预定义的 TASK 和 TASKEX 匹配
+  assert(fileName == TASK TASKEX);
+#endif
+}
+
+#ifdef USACO_LOCAL_JUDGE
+int debug_log = 0;
+int debug_assert = 0;
+#define MyPrintf(...)                   \
+  do {                                  \
+    if (debug_log) printf(__VA_ARGS__); \
+  } while (0)
+
+#define MyAssert(...)                      \
+  do {                                     \
+    if (debug_assert) assert(__VA_ARGS__); \
+  } while (0)
+#else
+#define MyPrintf(...)
+#define MyAssert(...)
+#endif
+
+constexpr int INF = 1 << 30;
+constexpr ll INFL = 1LL << 60;
+constexpr ll MOD = 1000000007;
+
+const double pi = acos(-1.0), eps = 1e-7;
+const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
+const int max3 = 2010, max4 = 20010, max5 = 200010, max6 = 2000010;
+
+template <class T>
+using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T>
+using max_queue = priority_queue<T>;
+
+void InitIO(int fileIndex) {  //
+// #define LOCAL_IO 1
+#ifdef USACO_LOCAL_JUDGE
+#define MAX_TIME 2000
+#ifdef LOCAL_IO
+#define USACO_TASK_FILE 0
+// #define TASKNO 20
+#ifdef TASKNO
+  fileIndex = TASKNO;
+#endif
+  string fileInName = string(TASK) + to_string(fileIndex) + ".in";
+  string fileOutName = string(TASK) + to_string(fileIndex) + ".out";
+  freopen(fileInName.c_str(), "r", stdin);
+  freopen(fileOutName.c_str(), "w", stdout);
+#endif
+#endif
+}
+
+vector<string> nums;
+int lenSum;
+string ans;
+
+// ansFlag: 当前 cur 是否肯定大于 ans
+void Dfs(int n, int offset, int ansFlag) {
+  if (n == -1) return;
+  char maxVal = nums[n].front();
+  for (int i = n; i >= 0; i--) {
+    if (nums[i].front() != maxVal) break;
+    // 尝试选择第 i 个元素
+    swap(nums[i], nums[n]);
+    if (ansFlag) {
+      memcpy(ans.data() + offset, nums[n].data(), nums[n].size());
+      Dfs(i - 1, offset + nums[n].size(), 1);
+      ansFlag = 0;  // 只有第一个可以直接覆盖，之后的需要再次比大小
+    } else {
+      int cmpFlag = strncmp(ans.data() + offset, nums[n].data(), nums[n].size());
+      if (cmpFlag < 0) {
+        memcpy(ans.data() + offset, nums[n].data(), nums[n].size());
+        Dfs(i - 1, offset + nums[n].size(), 1);
+      } else if (cmpFlag == 0) {  // 前缀相等
+        Dfs(i - 1, offset + nums[n].size(), 0);
+      } else {
+      }
+    }
+    swap(nums[i], nums[n]);
+  }
+}
+
+void Solver() {  //
+  int n;
+  scanf("%d", &n);
+  nums.resize(n);
+  lenSum = 0;
+  for (int i = 0; i < n; i++) {
+    ll x;
+    scanf("%lld", &x);
+    nums[i] = to_string(x);
+    lenSum += nums[i].size();
+  }
+  sort(nums.begin(), nums.end());
+  ans.resize(lenSum, '0');
+  Dfs(n - 1, 0, 1);
+  printf("%s\n", ans.c_str());
+}
+
+#ifdef USACO_LOCAL_JUDGE
+double costTime = 0;
+#endif
+void ExSolver() {
+#ifdef USACO_LOCAL_JUDGE
+  auto t1 = std::chrono::steady_clock::now();
+#endif
+  Solver();
+#ifdef USACO_LOCAL_JUDGE
+  auto t2 = std::chrono::steady_clock::now();
+  auto my = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+  costTime = my.count();
+#ifndef USACO_TASK_FILE
+  printf("my 用时: %.0lfms\n", costTime);
+#endif
+#endif
+}
+
+#ifdef USACO_TASK_FILE
+#include <unistd.h>
+
+#include <cstdio>
+int AC = 0;
+void DiffAns(int stdout_fd, int i) {
+  dup2(stdout_fd, STDOUT_FILENO);
+  close(stdout_fd);
+  stdout = fdopen(STDOUT_FILENO, "w");
+  int fileIndex = i;
+#ifdef TASKNO
+  fileIndex = TASKNO;
+#endif
+  string fileAns = string(TASK) + to_string(fileIndex) + ".ans";
+  string fileOut = string(TASK) + to_string(fileIndex) + ".out";
+  string cmd = string("diff -w " + fileAns + " " + fileOut + " > /dev/null");
+  if (system(cmd.c_str())) {
+    printf("case %d: Wrong answer, cost %.0lfms\n", i, costTime);
+  } else {
+    if (costTime > MAX_TIME) {
+      printf("case %d: Time Limit Exceeded, cost %.0lfms\n", i, costTime);
+    } else {
+      AC++;
+      printf("case %d: Accepted, cost %.0lfms\n", i, costTime);
+    }
+  }
+}
+void DiffSummary(int stdout_fd) {  // 统计通过的用例数量和得分
+  dup2(stdout_fd, STDOUT_FILENO);
+  close(stdout_fd);
+  stdout = fdopen(STDOUT_FILENO, "w");
+  printf("Total: %d / %d, 得分： %d\n", AC, USACO_TASK_FILE, AC * (100 / USACO_TASK_FILE));
+}
+#endif
+int main(int argc, char** argv) {
+  CheckUsacoTask();
+  int fileIndex = 1;
+#ifdef USACO_TASK_FILE
+  // 保存当前的 stdout 文件指针
+  int stdout_fd = dup(STDOUT_FILENO);
+  for (int i = 1; i <= USACO_TASK_FILE; i++) {
+    fileIndex = i;
+#endif
+    InitIO(fileIndex);
+    ExSolver();
+#ifdef USACO_TASK_FILE
+    fclose(stdout);
+    DiffAns(stdout_fd, i);
+    stdout_fd = dup(STDOUT_FILENO);
+  }
+  DiffSummary(stdout_fd);
+#endif
+  return 0;
+}
